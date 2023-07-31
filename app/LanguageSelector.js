157222dc10f1aa6a7b9ada6a02aeb12de61397c6
vijay-fe <vijaykumar.anchupogu@sportsit.net>
2023-07-31 21:15:08 +0530

    intital setup

---
+++ app/LanguageSelector.js
@@ -0,0 +1,69 @@
+import { useState, useRef, useEffect } from "react";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import data from "/public/flagsData.json";
+import Image from "next/image";
+import "@splidejs/react-splide/css";
+
+export default function LanguageSelector() {
+    const [activeCountry, setActiveCountry] = useState(null);
+    const splideRef = useRef(null);
+
+    useEffect(() => {
+        function handleActiveSlide() {
+            const activeSlide = splideRef.current.splide.Components.Elements.slides.filter(
+                (slide) => slide.classList.contains("is-active")
+            )[0];
+            if (activeSlide) {
+                const countryName = activeSlide.dataset.countryName;
+                setActiveCountry(countryName);
+            }
+        }
+
+        handleActiveSlide();
+
+        // Add event listener for the moved event
+        const splideInstance = splideRef.current.splide;
+        splideInstance.on("moved", handleActiveSlide);
+
+        // Remove the event listener when the component unmounts
+        return () => {
+            splideInstance.off("moved", handleActiveSlide);
+        };
+    }, []);
+
+    return (
+        <>
+            <Splide
+                // hasTrack={false}
+                options={{
+                    type: "loop",
+                    autoWidth: true,
+                    perPage: 1,
+                    arrows: false,
+                    gap: 20,
+                    focus: "center",
+                    pagination: false,
+                    autoplay: true,
+                    live: true,
+                    lazyLoad: "nearby",
+                }}
+                className="splide__container"
+                ref={splideRef}
+            >
+                {data &&
+                    data.map((country) => {
+                        return (
+                            <SplideSlide key={country.id} className="py-2" data-country-name={country.name}>
+                                <div className="country_image">
+                                    <Image src={`/${country.image}`} alt={country.name} width={100} height={100} />
+                                </div>
+                            </SplideSlide>
+                        );
+                    })}
+            </Splide>
+            <div className="text-center font-semibold text-2xl">
+                {activeCountry || ""}
+            </div>
+        </>
+    );
+}
